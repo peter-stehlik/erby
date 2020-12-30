581350f20b9acdc78934ee77da1fde1db334ba2f
Peter Stehlik <peter@inovative.sk>
2020-12-30 10:32:32 +0100

    init repo

---
+++ web/assets/js/custom.js
@@ -0,0 +1,28 @@
+/**
+ * 	
+ * CUSTOM JS SNIPPETS
+ * vanilla js, please
+ * 
+ */
+let Custom = {
+
+	/**
+	 * handle navigation
+	 */
+	toggleSidebar: () => {
+
+	},
+
+	/**
+	 * manage live search
+	 * when typing
+	 */
+	initAutocomplete: () => {
+
+	}
+
+}
+
+/* INIT FUNCTIONS */
+Custom.toggleSidebar();
+Custom.initAutocomplete();
