42274fb37012e9c6d310f9aa153e6b7da4446bae
miroviazanko <miro.viazanko@gmail.com>
2020-12-31 11:32:37 +0100

    desktop verzia bez inputu

--- web/assets/js/custom.js
+++ web/assets/js/custom.js
@@ -1,15 +1,49 @@
 /**
- * 	
+ *
  * CUSTOM JS SNIPPETS
  * vanilla js, please
- * 
+ *
  */
+
 let Custom = {
 
 	/**
 	 * handle navigation
 	 */
 	toggleSidebar: () => {
+		const addIcon = document.getElementsByClassName('menu-erb-icon');
+		const searchIcon = document.getElementsByClassName('menu-search-icon');
+		const infoIcon = document.getElementsByClassName('menu-info-icon')
+
+		const addText = document.getElementsByClassName('menu-add-text');
+		const infoText = document.getElementsByClassName('menu-info-text');
+		const searchText = document.getElementsByClassName('menu-search-text');
+
+
+		const arrayOfIcons = [addIcon[0], searchIcon[0], infoIcon[0]];
+		const arraOfTexts = [addText[0], searchText[0], infoText[0]];
+
+		arrayOfIcons.map( (icon,i) => {
+			icon.addEventListener('mousemove', function() {
+				showText(i)
+			})
+
+			icon.addEventListener('mouseout', function () {
+				hideText(i)
+			})
+		})
+
+
+		function showText(index) {
+			arraOfTexts[index].style.color = ' #333333';
+		}
+
+
+		function hideText(index) {
+			arraOfTexts[index].style.color = ' #ffffff';
+		}
+
+		console.log(arrayOfIcons, arraOfTexts);
 
 	},
 
